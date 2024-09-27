9ecf7f4bf72b8ce4f920cb0fc0c9629423be1b27
Andrey <111575820+andrey-918@users.noreply.github.com>
2024-09-27 22:12:17 +0300

    birth and RelationshipGoal user.go

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,17 +7,19 @@ import (
 //go:generate mockgen -source=*.go -destination=*_mock.go -package=*
 
 type User struct {
-	ID        string    `json:"id" validate:"required"`
-	Name      string    `json:"name" validate:"required,min=2,max=50"`
-	Age       int       `json:"age" validate:"required,min=18,max=100"`
-	Gender    string    `json:"gender" validate:"required,oneof=male female"`
-	Email     string    `json:"email" validate:"required,regexp=^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"`
-	Phone     string    `json:"phone" validate:"required,regexp=^(\\+|[0-9])([0-9]*)$, min=11, max=12"`
-	Bio       string    `json:"bio,omitempty" validate:"max=150"`
-	Interests []string  `json:"interests,omitempty"`
-	Location  string    `json:"location,omitempty"`
-	CreatedAt time.Time `json:"created_at" validate:"required"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	ID               string    `json:"id" validate:"required"`
+	Name             string    `json:"name" validate:"required,min=2,max=50"`
+	Age              int       `json:"age" validate:"required,min=18,max=100"`
+	Gender           string    `json:"gender" validate:"required,oneof=male female"`
+	Email            string    `json:"email" validate:"required,regexp=^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"`
+	Phone            string    `json:"phone" validate:"required,regexp=^(\\+|[0-9])([0-9]*)$, min=11, max=12"`
+	Bio              string    `json:"bio,omitempty" validate:"max=150"`
+	Interests        []string  `json:"interests,omitempty"`
+	Location         string    `json:"location,omitempty"`
+	CreatedAt        time.Time `json:"created_at" validate:"required"`
+	UpdatedAt        time.Time `json:"updated_at,omitempty"`
+	DateOfBirth      time.Time `json:"date_of_birth" validate:"required"`
+	RelationshipGoal string    `json:"relationship_goal" validate:"required,oneof=dating friendship relationship"`
 }
 
 type Match struct {
