7be19c2f2344686ddf6f47787b565b0eec450803
KranjQ <easypandaoo@gmail.com>
2024-10-28 00:15:54 +0300

    add sqlBuilder

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module sparkit
 go 1.22.5
 
 require (
+	github.com/Masterminds/squirrel v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/rs/cors v1.11.1 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
--- internal/repo/user/postgresql.go
+++ internal/repo/user/postgresql.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	sq "github.com/Masterminds/squirrel"
 	"sparkit/internal/models"
 )
 
@@ -16,12 +17,22 @@ func New(db *sql.DB) *Storage {
 }
 
 func (repo *Storage) AddUser(ctx context.Context, user models.User) error {
-	_, err := repo.DB.Exec("INSERT INTO users (username, password, age, gender) VALUES ($1, $2, $3, $4)", user.Username, user.Password, user.Age, user.Gender)
-	return fmt.Errorf("AddUser err : %v: ", err)
+	//_, err := repo.DB.Exec("INSERT INTO users (username, password, age, gender) VALUES ($1, $2, $3, $4)", user.Username, user.Password, user.Age, user.Gender)
+	sqlReq, args, err := sq.Insert("users").
+		Columns("username", "password", "age", "gender").
+		Values(user.Username, user.Password, user.Age, user.Gender).ToSql()
+	_, err = repo.DB.Exec(sqlReq, args)
+	if err != nil {
+		return fmt.Errorf("AddUser err : %v: ", err)
+	}
+	return nil
 }
 
 func (repo *Storage) DeleteUser(ctx context.Context, username string) error {
-	_, err := repo.DB.Exec("DELETE FROM users WHERE username=$1", username)
+	//_, err := repo.DB.Exec("DELETE FROM users WHERE username=$1", username)
+	sqlReq, args, err := sq.Delete("users").
+		Where(sq.Eq{"username": username}).ToSql()
+	_, err = repo.DB.Exec(sqlReq, args)
 	if err != nil {
 		return fmt.Errorf("DeleteUser err: %v", err)
 	}
@@ -30,7 +41,12 @@ func (repo *Storage) DeleteUser(ctx context.Context, username string) error {
 
 func (repo *Storage) GetUserByUsername(ctx context.Context, username string) (models.User, error) {
 	var user models.User
-	err := repo.DB.QueryRow("SELECT id, username, password FROM users WHERE username=$1", username).Scan(&user.ID, &user.Username, &user.Password)
+	//err := repo.DB.QueryRow("SELECT id, username, password FROM users WHERE username=$1", username).Scan(&user.ID, &user.Username, &user.Password)
+	sqlReq, args, err := sq.Select("users").
+		Columns("id", "username", "password").
+		Where(sq.Eq{"username": username}).
+		ToSql()
+	err = repo.DB.QueryRow(sqlReq, args).Scan(&user.ID, &user.Username, &user.Password)
 	if err != nil {
 		return models.User{}, fmt.Errorf("GetUserByUsername err: %v", err)
 	}
@@ -39,7 +55,14 @@ func (repo *Storage) GetUserByUsername(ctx context.Context, username string) (mo
 
 func (repo *Storage) GetUserList(ctx context.Context) ([]models.User, error) {
 	var users []models.User
-	rows, err := repo.DB.Query("SELECT id, username, age, gender FROM users")
+	sqlReq, args, sqErr := sq.Select("users").
+		Columns("id", "username", "age", "gender").
+		ToSql()
+	if sqErr != nil {
+		return []models.User{}, fmt.Errorf("GetUserList err: %v", sqErr)
+	}
+	//err = repo.DB.QueryRow(sqlReq, args).Scan(&user.ID, &user.Username, &user.Password)
+	rows, err := repo.DB.Query(sqlReq, args)
 	if err != nil {
 		return []models.User{}, fmt.Errorf("GetUserList err: %v", err)
 	}
